81e3452320d2758d60f020bbfaaa48920b2f742a
XxSergioLDCxX <sergiolopezdecoca@gmail.com>
2023-11-22 20:03:49 +0100

    Formulario de Registro

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'start_at' => now(),
+        ]);
+
+        return response()->json($user, 201);
+    }
+}

de636d7ddac9c77c0b8837f81a362074c329ac03
XxSergioLDCxX <119493186+XxSergioLDCxX@users.noreply.github.com>
2023-11-29 13:31:52 +0100

    merge dev

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------

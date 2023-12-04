77afdf7fd44e62fbc9cd14f00753c6009c733772
bhamidou <badrhamidou@gmail.com>
2023-12-04 13:13:02 +0100

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\LoteUser;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,74 +23,92 @@ use App\Http\Controllers\AuthController;
 
 Route::group(['middleware' => ['cors']], function () {
 
-    Route::controller(LoteController::class)->group(function () {
-        Route::get('/mis-lotes/{userId}', 'index');
-    });
-    Route::prefix('lote')->group(function () {
-        Route::controller(LoteController::class)->group(function () {
-            Route::get('/disponibles', 'disponible');
-            Route::get('/{loteId}', 'show');
-            Route::get('/{loteId}/rechazar', 'rechazar');
-            Route::post('{loteId}/clasificar', 'clasificar');
-            Route::patch('/{loteId}', 'update');
-            Route::post('/', 'store');
-        });
 
+    Route::get('empresa/mis-lotes/', [LoteController::class,'index']);
+    Route::get('colaborador/mis-lotes/', [LoteUser::class,'index']);
+        /**
+         * @author: badr
+         * Rutas para CRUD lote.
+         * */
+        Route::prefix('lote')->group(function () {
+            Route::controller(LoteController::class)->group(function () {
+                Route::get('/disponibles', 'disponible');
+                Route::get('/{loteId}', 'show');
+                Route::get('/{loteId}/rechazar', 'rechazar');
+                Route::post('{loteId}/clasificar', 'clasificar');
+                Route::patch('/{loteId}', 'update');
+                Route::post('/', 'store');
+            });
+
+        });
+        
+        Route::prefix('user')->group(function () {
         Route::controller(ClasificadorController::class)->group(function () {
             Route::post('{userId}/asign', 'store');
         });
-    });
 
-    Route::prefix('user')->group(function () {
+        //
+
+        /**
+         * @author: badr
+         * Rutas que un usuario puede realizar.
+         * */
         Route::controller(ClasificadorController::class)->group(function () {
             Route::get('/{id}/lotes', 'show');
             Route::post('/lote', 'store');
-            Route::get('/componentes', [ComponentesController::class, 'index']);
+        });
+        /**
+         * @author: badr
+         * Rutas que un usuario puede hacer con componentes.
+         * */
+            Route::controller(ComponentesController::class)->group(function () {
+                Route::get('{userId}/componentes', 'index');
+                Route::get('{userId}/componentes/{id}', 'show');
+                Route::put('{userId}/componentes/{id}', 'update');
+                Route::post('componete', 'store');
+                Route::delete('{userId}/componentes/{id}', 'destroy');
+            });
+
             Route::post('/login', [AuthController::class, 'login']);
 
             Route::post('/register', [AuthController::class, 'register']);
             Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
                 return $request->user();
             });
-        });
-    });
-    Route::post('/users', [UserController::class, 'store']);
-    Route::prefix('user/lote')->group(function () {
-        Route::controller(LoteUserController::class)->group(function () {
-            Route::get('/', 'index');
-            Route::get('/{id}', 'show');
-            Route::patch('/{loteId}', 'update');
-            Route::post('/', 'store');
-            Route::delete('/{id}', 'destroy');
-        });
-    });
-
-
-    Route::prefix('user')->group(function () {
-        Route::controller(UserController::class)->group(function () {
-            Route::get('/{id}/mis-roles', 'roles');
-            Route::get('/', 'index');
-            Route::get('/{id}', 'show');
-            Route::put('/{id}', 'update');
-            Route::post('/', 'store');
-            Route::delete('/{id}', 'destroy');
-
-            Route::prefix('{userId}/lote')->group(function () {
-                Route::controller(LoteController::class)->group(function () {
-                    Route::get('/', 'show');
+            Route::controller(UserController::class)->group(function () {
+                Route::get('/{id}/mis-roles', 'roles');
+                Route::get('/', 'index');
+                Route::get('/{id}', 'show');
+                Route::put('/{id}', 'update');
+                Route::post('/', 'store');
+                Route::delete('/{id}', 'destroy');
+    
+                Route::prefix('{userId}/lote')->group(function () {
+                    Route::controller(LoteController::class)->group(function () {
+                        Route::get('/', 'show');
+                    });
                 });
             });
         });
-    });
 
-    Route::prefix('componentes')->group(function () {
         Route::controller(ComponentesController::class)->group(function () {
-
-            Route::get('/', 'index');
-            Route::get('/{id}', 'show');
-            Route::put('/{id}', 'update');
-            Route::post('/', 'store');
-            Route::delete('/{id}', 'destroy');
+            Route::get('/componentes', 'index');
+            Route::get('/componentes/{id}', 'show');
+            Route::put('/componentes/{id}', 'update');
+            Route::post('componete', 'store');
+            Route::delete('/componentes/{id}', 'destroy');
         });
     });
-});
+
+
+    // Route::post('/users', [UserController::class, 'store']);
+    // Route::prefix('user/lote')->group(function () {
+    //     Route::controller(LoteUserController::class)->group(function () {
+    //         Route::get('/', 'index');
+    //         Route::get('/{id}', 'show');
+    //         Route::patch('/{loteId}', 'update');
+    //         Route::post('/', 'store');
+    //         Route::delete('/{id}', 'destroy');
+    //     });
+    // });
+// });

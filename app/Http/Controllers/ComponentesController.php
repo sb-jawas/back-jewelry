7c6d422a57843ea221f8de5c0ac398b7126a185a
bhamidou <badrhamidou@gmail.com>
2023-12-07 14:01:16 +0100

    add funcs to show componentes by user id and all componentes

--- app/Http/Controllers/ComponentesController.php
+++ app/Http/Controllers/ComponentesController.php
@@ -4,24 +4,89 @@ namespace App\Http\Controllers;
 
 use App\Models\Componentes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * @author: badr => @bhamidou
+ */
 
 class ComponentesController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Mostrar todos los componentes en función del usuario.
      */
-    public function index()
+    public function index(String $userId)
     {
+        $vecValidator = [
+            "user_id" => $userId
+        ];
+
+        $message = [
+            "user_id" => [
+                "exists" => "Este usuario no existe",
+            ]
+        ];
+
+        $validator = Validator::make($vecValidator, [
+            'user_id' => 'exists:users,id',
+        ], $message);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),404);
+        }
+
+        $componentes = DB::select('select * from componentes where created_user_id = ?', [$userId]);
+        return response()->json($componentes);
+    }
+
+    public function allComponentes(){
         $componentes = Componentes::all();
         return response()->json($componentes);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Crear un nuevo componente.
      */
-    public function store(Request $request)
+    public function store(Request $req)
     {
-        //
+        $message =[
+            'created_user_id' =>[
+                "required" => "Es necesario el usuario",
+                "exists" => "Este usuario no existe"
+            ],
+            'name' => [
+                "required" => "Es necesario un nombre",
+                "min" => "Como mínimo se permite un solo caracter",
+                "max" => "Como máximo solo se permite 30 caracteres",
+
+            ],
+            'desc' => [
+                "required" => "Es necesaria una descripción",
+                "min" => "Como mínimo se permite un solo caracter",
+                "max" => "Como máximo solo se permite 255 caracteres",
+            ],
+            'is_hardware' => [
+                "required" => "Es necesario indicar si es de tipo hardware o no",
+                "integer" => "Tiene que ser un número entero",
+                "between" => "Solo se permite 0 ó 1",
+            ],
+        ];
+        $validator = Validator::make($req->all(), [
+            'created_user_id' => 'required|exists:users,id',
+            'name' => 'required|min:1|max:30',
+            'desc' => 'required|min:1|max:255',
+            'is_hardware' => 'required|integer|between:0,1',
+
+        ], $message);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),404);
+        }
+
+        $componente = Componentes::create($req->all());
+        return response()->json($componente);
+
     }
 
     /**

9b8404c2deeb07aee1f6ee566612fd46f50659f9
bhamidou <badrhamidou@gmail.com>
2023-12-12 18:25:54 +0100

    Add try catch to Colaborador Controller

--- app/Http/Controllers/Lotes/ColaboradorController.php
+++ app/Http/Controllers/Lotes/ColaboradorController.php
@@ -7,15 +7,17 @@ use App\Models\Lote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+
 /**
  * @author: Badr => @bhamidou
  */
 
 class ColaboradorController extends Controller
 {
-    public function index(String $userId){
+    public function index(String $userId)
+    {
 
-        $vecValidator = ["user_id"=>$userId];
+        $vecValidator = ["user_id" => $userId];
         $messages = [
             'exists' => 'Este usuario no existe'
         ];
@@ -24,19 +26,20 @@ class ColaboradorController extends Controller
 
         ], $messages);
 
-        if($validator->fails()){
-            return response()->json($validator->errors(),202);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 202);
         }
 
         $lotesUser = DB::select('select lote.id, lote.status_code_id, lote.created_at, status_code.name from lote inner join status_code on status_code.id = lote.status_code_id where user_id = ?', [$userId]);
 
-        return response()->json($lotesUser,200);
+        return response()->json($lotesUser, 200);
     }
 
-    public function show(String $userId,String $loteId){
+    public function show(String $userId, String $loteId)
+    {
         $vecValidator = [
-            "user_id"=>$userId,
-            "lote_id"=>$loteId
+            "user_id" => $userId,
+            "lote_id" => $loteId
         ];
         $messages = [
             'user_id' => [
@@ -52,20 +55,21 @@ class ColaboradorController extends Controller
 
         ], $messages);
 
-        if($validator->fails()){
-            return response()->json($validator->errors(),404);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 404);
         }
 
-        $showLote = DB::select('select lote.id, lote.lat, lote.long, lote.observation, lote.status_code_id as "status_code", status_code.name as "status",lote.created_at from lote inner join status_code on status_code.id = lote.status_code_id where lote.id = ? and user_id = ?',[$loteId, $userId]);
+        $showLote = DB::select('select lote.id, lote.lat, lote.long, lote.observation, lote.status_code_id as "status_code", status_code.name as "status",lote.created_at from lote inner join status_code on status_code.id = lote.status_code_id where lote.id = ? and user_id = ?', [$loteId, $userId]);
         $statusCode = 200;
-        if($showLote == null){
+        if ($showLote == null) {
             $statusCode = 404;
         };
 
         return response()->json($showLote[0], $statusCode);
     }
 
-    public function store(Request $req){
+    public function store(Request $req)
+    {
         $messages = [
             'user_id' => [
                 'required' => 'Es obligatorio el usuario'
@@ -84,57 +88,61 @@ class ColaboradorController extends Controller
 
         ], $messages);
 
-        if($validator->fails()){
-            return response()->json($validator->errors(),400);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
+        try {
 
-        $newLote = LoteController::store($req);
+            $newLote = LoteController::store($req);
 
-        return response()->json($newLote);
+            return response()->json($newLote);
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
+        }
     }
 
-    public function cancelar(Request $req, String $loteId){
-
-        $vecValidator = [
-            "user_id"=> $req->get("user_id"),
-            "lote_id"=> $loteId
-        ];
-
+    public function cancelar(Request $req)
+    {
         $messages = [
-                "user_id" => [
-                    "required"=>"Es necesario un usuario",
-                    "exists"=>"Este usuario no existe",
-                ],
-                "lote_id" => [
-                    "required"=>"Es necesario el id del lote",
-                    "exists"=>"Este lote no exite",
-                ]
+            "user_id" => [
+                "required" => "Es necesario un usuario",
+                "exists" => "Este usuario no existe",
+            ],
+            "lote_id" => [
+                "required" => "Es necesario el id del lote",
+                "exists" => "Este lote no exite",
+            ]
         ];
 
-        $validator = Validator::make($vecValidator, [
+        $validator = Validator::make($req->all(), [
             'user_id' => 'required|exists:users,id',
             'lote_id' => 'required|exists:lote,id',
         ], $messages);
 
-        if($validator->fails() ){
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
-        $lote = Lote::find($loteId);
-        
-        $check = true;
-        if($lote->user_id == $req->get("user_id")){
-            $check = false;
+        try {
+
+            $lote = Lote::find($req->get('lote_id'));
+
+            $check = true;
+            if ($lote->user_id == $req->get("user_id")) {
+                $check = false;
+            }
+            if ($check) {
+                $rtnObj = ["msg" => "No es posible cancelar un lote que no ha creado"];
+
+                return response()->json($rtnObj, 404);
+            }
+
+            $lote->status_code_id = 7;
+
+            $lote->save();
+
+            return response()->json($lote, 200);
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
         }
-        if ($check) {
-            $rtnObj = ["msg"=>"No es posible cancelar un lote que no ha creado"];
-
-            return response()->json($rtnObj, 404);
-        }
-
-        $lote->status_code_id = 7;
-
-        $lote->save();
-
-        return response()->json($lote, 200);
     }
 }

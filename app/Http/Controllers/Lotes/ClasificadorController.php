8d6f1de47598a7f046ec028893cb6d1f30b38898
bhamidou <badrhamidou@gmail.com>
2023-12-12 18:24:49 +0100

    Add try catch clasficador controller

--- app/Http/Controllers/Lotes/ClasificadorController.php
+++ app/Http/Controllers/Lotes/ClasificadorController.php
@@ -88,22 +88,25 @@ class ClasificadorController extends Controller
         $loteUser->lote_id = $req->get("lote_id");
 
         $lote = $req->get("lote_id");
-
-        if (count($req->get("lote_id")) == 1) {
-            $this->saveLote($req->get("lote_id")[0], $userId);
-        } else {
-            $i = 0;
-            while ($i < count($req->get("lote_id"))) {
-                $this->saveLote($req->get("lote_id")[$i], $userId);
-                $i++;
+        try {
+            if (count($req->get("lote_id")) == 1) {
+                $this->saveLote($req->get("lote_id")[0], $userId);
+            } else {
+                $i = 0;
+                while ($i < count($req->get("lote_id"))) {
+                    $this->saveLote($req->get("lote_id")[$i], $userId);
+                    $i++;
+                }
             }
+            $lote = Lote::find($req->get("lote_id"));
+            foreach ($lote as $key) {
+                $key->status_code_id = 4;
+                $key->save();
+            }
+            return response()->json(["msg" => "Asignado correctamente!", "store" => $loteUser]);
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
         }
-        $lote = Lote::find($req->get("lote_id"));
-        foreach ($lote as $key) {
-            $key->status_code_id = 4;
-            $key->save();
-        }
-        return response()->json(["msg" => "Asignado correctamente!", "store" => $loteUser]);
     }
 
     /**
@@ -126,7 +129,7 @@ class ClasificadorController extends Controller
         ], $message);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(),404);
+            return response()->json($validator->errors(), 404);
         }
 
         $lote = Lote::find($loteId);
@@ -145,9 +148,8 @@ class ClasificadorController extends Controller
 
         ];
 
-
         return $rtnObj;
-        return response()->json($lote,200);
+        return response()->json($lote, 200);
     }
 
     /**
@@ -169,22 +171,27 @@ class ClasificadorController extends Controller
         ], $message);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(),404);
+            return response()->json($validator->errors(), 404);
         }
 
 
         $despiece = DB::select('select componentes_has_lote.cantidad as cantidad, componentes.name as componente, componentes_has_lote.observation, (select name from users where id = componentes_has_lote.clasificador_id) as "clasificador_id" from componentes_has_lote inner join lote on componentes_has_lote.lote_id = lote.id  inner join componentes on componentes_has_lote.componente_id = componentes.id  inner join users on users.id = lote.user_id where lote_id = ?', [$loteId]);
-        
+
 
         return response()->json($despiece, 200);
     }
 
     private function saveLote($loteId, $userId)
     {
-        $loteUser = new LoteUser;
-        $loteUser->user_id = $userId;
-        $loteUser->lote_id = $loteId;
-        $loteUser->save();
+        try {
+
+            $loteUser = new LoteUser;
+            $loteUser->user_id = $userId;
+            $loteUser->lote_id = $loteId;
+            $loteUser->save();
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
+        }
     }
 
     public function clasificar(Request $req, $loteId)
@@ -194,52 +201,60 @@ class ClasificadorController extends Controller
         $clasificador = $req->get("user_id");
         $obs = $req->get("observation");
 
+        try {
+            $i = 0;
+            while ($i < count($arrIds)) {
+                $despiece = new LoteDespiece;
+                $despiece->cantidad = $arrCantidad[$i];
+                $despiece->clasificador_id = $clasificador;
+                $despiece->lote_id = $loteId;
+                $despiece->componente_id = $arrIds[$i];
+                $despiece->observation = $obs[$i];
+                $despiece->save();
 
-        $i = 0;
-        while ($i < count($arrIds)) {
-            $despiece = new LoteDespiece;
-            $despiece->cantidad = $arrCantidad[$i];
-            $despiece->clasificador_id = $clasificador;
-            $despiece->lote_id = $loteId;
-            $despiece->componente_id = $arrIds[$i];
-            $despiece->observation = $obs[$i];
-            $despiece->save();
+                $inventario = DB::select('select id from inventario where componente_id = ? ', [$despiece->componente_id]);
 
-            $inventario = DB::select('select id from inventario where componente_id = ? ',[$despiece->componente_id]);
+                if ($inventario == null) {
+                    $newInventario = new Inventario;
+                    $newInventario->cantidad = $despiece->cantidad;
+                    $newInventario->componente_id = $despiece->componente_id;
+                    $newInventario->save();
+                } else {
+                    $updateInventario = Inventario::find($inventario[0]->id);
+                    $updateInventario->cantidad += $despiece->cantidad;
+                    $updateInventario->save();
+                }
 
-            if($inventario == null){
-                $newInventario = new Inventario;
-                $newInventario->cantidad = $despiece->cantidad;
-                $newInventario->componente_id = $despiece->componente_id;
-                $newInventario->save();
-            }else{
-                $updateInventario = Inventario::find($inventario[0]->id);
-                $updateInventario->cantidad += $despiece->cantidad;
-                $updateInventario->save();
+                $i++;
             }
-            
-            $i++;
-        }
-        
-        $lote = Lote::find($loteId);
-        $lote->status_code_id = 5;
-        $lote->save();
 
-        $rtnMsg = ["message" => "Lote clasificado correctamente", "despiece" => $despiece];
-        return response()->json($rtnMsg);
+            $lote = Lote::find($loteId);
+            $lote->status_code_id = 5;
+            $lote->save();
+
+            $rtnMsg = ["message" => "Lote clasificado correctamente", "despiece" => $despiece];
+            return response()->json($rtnMsg);
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
+        }
     }
 
     public function rechazar(String $loteId)
     {
-        $lote = Lote::find($loteId);
-        $lote->status_code_id = 6;
-        $lote->save();
-        return response()->json($lote);
+        try {
+
+            $lote = Lote::find($loteId);
+            $lote->status_code_id = 6;
+            $lote->save();
+            return response()->json($lote);
+        } catch (\Exception $exception) {
+            return response()->json(["msg" => $exception->getMessage()], 500);
+        }
     }
 
-    public function todos(){
+    public function todos()
+    {
         $lotes = DB::select('select lote.id, users.name_empresa as "empresa", lote.created_at, status_code.name as "status" from lote inner join users on users.id = lote.user_id inner join status_code on status_code.id = lote.status_code_id');
         return response()->json($lotes, 200);
-
     }
 }
